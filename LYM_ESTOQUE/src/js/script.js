c826889fa56df8df17be08f7c8bf4d45db842e8a
Abndrey <160252981+Abndrey@users.noreply.github.com>
2024-11-07 21:38:38 -0300

    Add files via upload

---
+++ LYM_ESTOQUE/src/js/script.js
@@ -0,0 +1,135 @@
+function salvar() {
+
+    const nome_alimento = document.getElementById('nome_alimento').value;
+  
+    console.log(nome_alimento);
+  
+    if (nome_alimento == "") {
+    alert ("nome não informado");
+    getElementById('nome_alimento').focus ();
+    return;
+  }
+  
+  if ( nome_alimento.length < 2) {
+     alert("o nome do alimento deve conter no minimo 2 caractere.");
+    return;
+  }
+  
+  // const regex = /^[A-Za-zÀ-ÿ\s]+$/;
+  // return regex.test(nome); }
+  
+  
+  if (nome_alimento.length > 50){
+          alert("o nome deve ter no máximo 50 caracteres");
+      return;
+  }
+  
+  
+    var headers = new Headers();
+  
+  
+    headers.append("Content-Type", "application/json");
+    headers.append('Access-Control-Allow-Origin', '*');
+  
+    fetch('http://127.0.0.1:8080/alimento/insert', {
+  
+      method: "POST",
+      mode: "cors", // Usando 'cors' para permitir a requisição de origem cruzada
+      cache: "no-cache",
+  
+      // Convertendo o objeto JavaScript para JSON
+      // Esta parte é importante onde você deve passar os parametros (dados) da sua tela
+      body: JSON.stringify({ nome: nome_alimento }),
+  
+      headers: headers
+  
+      //Aqui inicia função then
+    }).then(response => {
+  
+      if (response.ok) {
+  
+        //Esta linha imprime a mensagem no concole
+        console.log('Foi no servidor e voltou');
+  
+        //Esta linha carrega a página sucesso
+        window.location.href = 'sucesso.html'
+      } else {
+        //Esta linha imprime a mensagem no console
+        console.log('Aconteceu algo que não foi possivel salvar');
+  
+        //Esta linha imprime a mensagem de erro
+        throw new Error('Erro ao tentar salvar');
+      }
+  
+    })
+  
+      //Aqui será executado caso a then não seja chamado
+      //Aqui será executado caso a then não seja chamado
+  
+      //Aqui será executado caso a then não seja chamado
+      .catch(error => console.error('Erro!:', error));
+  
+  }
+  
+  function consultar() {
+  
+    const alimento = document.getElementById('nome_alimento').value;
+  
+  
+  
+    console.log(alimento);
+  
+    var headers = new Headers();
+  
+    headers.append("Content-Type", "application/json");
+    headers.append('Access-Control-Allow-Origin', '*');
+  
+    fetch('http://127.0.0.1:8080/alimento/findByNome', {
+  
+      method: "POST",
+      mode: "cors", // Usando 'cors' para permitir a requisição de origem cruzada
+      cache: "no-cache",
+  
+      // Convertendo o objeto JavaScript para JSON
+      // Esta parte é importante onde você deve passar os parametros (dados) da sua tela
+      body: JSON.stringify({ nome: alimento }),
+  
+      headers: headers
+  
+      //Aqui inicia função then
+    }).then(response => {
+  
+      if (response.ok) {
+  
+        return response.text();
+        //Esta linha imprime a mensagem no concole
+        //console.log('Foi no servidor e voltou');
+  
+        //Esta linha carrega a página sucesso
+        //window.location.href = 'sucesso.html'
+      } else {
+        //Esta linha imprime a mensagem no console
+        console.error('Erro na resposta da API');
+  
+        //Esta linha imprime a mensagem de erro
+        throw new Error('Erro ao tentar buscar o alimento');
+      }
+  
+    })
+  
+      .then(id_alimento => {
+  
+        console.log("ID do alimento recebido:", id_alimento);
+  
+        if (id_alimento) {
+          localStorage.setItem('id_alimento', id_alimento);
+          alert("Alimento achado com sucesso, agora você pode alterá-lo");
+        } else {
+          localStorage.setItem('id_alimento', '');
+          console.error("Id não encontrado na resposta");
+        }
+      })
+      .catch(error => {
+        console.error("Erro capturado no catch", error);
+      });
+  }
